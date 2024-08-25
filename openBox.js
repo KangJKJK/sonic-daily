a9ff5dfbaea1ec61e2cbb240f0ba033fbc8a8fa7
kangjk <96231886+KangJKJK@users.noreply.github.com>
2024-08-25 21:39:24 +0900

    openBox.js

--- openBox.js
+++ openBox.js
@@ -1,7 +1,5 @@
-import { Transaction } from '@solana/web3.js';
 import fetch from 'node-fetch';
 
-// 공통 헤더를 설정하는 객체
 const defaultHeaders = {
     'accept': '*/*',
     'accept-language': 'en-US,en;q=0.7',
@@ -21,13 +19,18 @@ export const openBox = async (keyPair, auth) => {
     let success = false;
     while (!success) {
         try {
-            // 미스터리 박스 거래 생성 요청
-            const data = await fetch('https://odyssey-api.sonic.game/user/rewards/mystery-box/build-tx', {
+            const response = await fetch('https://odyssey-api.sonic.game/user/rewards/mystery-box/build-tx', {
                 headers: {
                     ...defaultHeaders,
                     'authorization': auth
                 }
-            }).then(res => res.json());
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+
+            const data = await response.json();
 
             if (data.data) {
                 const transactionBuffer = Buffer.from(data.data.hash, 'base64');
@@ -38,20 +41,26 @@ export const openBox = async (keyPair, auth) => {
                 const signature = await sendTransaction(transaction, keyPair);
 
                 // 미스터리 박스 개봉 요청
-                const open = await fetch('https://odyssey-api.sonic.game/user/rewards/mystery-box/open', {
+                const openResponse = await fetch('https://odyssey-api.sonic.game/user/rewards/mystery-box/open', {
                     method: 'POST',
                     headers: {
                         ...defaultHeaders,
                         'authorization': auth
                     },
                     body: JSON.stringify({ 'hash': signature })
-                }).then(res => res.json());
+                });
+
+                if (!openResponse.ok) {
+                    throw new Error(`HTTP error! status: ${openResponse.status}`);
+                }
+
+                const openData = await openResponse.json();
 
                 success = true;
-                return `성공적으로 미스터리 박스를 열었습니다, ${open.data.reward}!`;
+                return `성공적으로 미스터리 박스를 열었습니다, ${openData.data.reward}!`;
             }
         } catch (e) {
-            console.error('미스터리 박스 개봉 오류:', e);
+            console.error('미스터리 박스 개봉 오류:', e.message);
             await new Promise(resolve => setTimeout(resolve, 5000)); // 5초 대기 후 재시도
         }
     }
