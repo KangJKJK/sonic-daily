9cfab6b1985c66179838d12f46ef62e40770cd45
kangjk <96231886+KangJKJK@users.noreply.github.com>
2024-08-25 21:35:36 +0900

    openBox.js

--- openBox.js
+++ openBox.js
@@ -1,31 +1,47 @@
 import { Transaction } from '@solana/web3.js';
 import fetch from 'node-fetch';
 
+// 공통 헤더를 설정하는 객체
+const defaultHeaders = {
+    'accept': '*/*',
+    'accept-language': 'en-US,en;q=0.7',
+    'content-type': 'application/json',
+    'priority': 'u=1, i',
+    'sec-ch-ua': '"Not/A)Brand";v="8", "Chromium";v="126", "Brave";v="126"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-site',
+    'sec-gpc': '1',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'
+};
+
 export const openBox = async (keyPair, auth) => {
     let success = false;
     while (!success) {
         try {
+            // 미스터리 박스 거래 생성 요청
             const data = await fetch('https://odyssey-api.sonic.game/user/rewards/mystery-box/build-tx', {
                 headers: {
-                    'accept': '*/*',
-                    'accept-language': 'en-US,en;q=0.7',
-                    'content-type': 'application/json',
+                    ...defaultHeaders,
                     'authorization': auth
                 }
             }).then(res => res.json());
 
             if (data.data) {
-                const transactionBuffer = Buffer.from(data.data.hash, "base64");
+                const transactionBuffer = Buffer.from(data.data.hash, 'base64');
                 const transaction = Transaction.from(transactionBuffer);
                 transaction.partialSign(keyPair);
+
+                // 트랜잭션 전송
                 const signature = await sendTransaction(transaction, keyPair);
 
+                // 미스터리 박스 개봉 요청
                 const open = await fetch('https://odyssey-api.sonic.game/user/rewards/mystery-box/open', {
                     method: 'POST',
                     headers: {
-                        'accept': '*/*',
-                        'accept-language': 'en-US,en;q=0.7',
-                        'content-type': 'application/json',
+                        ...defaultHeaders,
                         'authorization': auth
                     },
                     body: JSON.stringify({ 'hash': signature })
