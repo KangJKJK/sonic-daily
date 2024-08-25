f88544925f78d3d3a78ffd521b349c6213a229bb
kangjk <96231886+KangJKJK@users.noreply.github.com>
2024-08-25 19:40:59 +0900

    sonic-daily.sh

--- sonic-daily.sh
+++ sonic-daily.sh
@@ -205,7 +205,6 @@ const openBox = async (keyPair, auth) => {
         const keypair = getKeypairFromPrivateKey(privateKey); // 개인키로 Keypair 객체를 생성합니다.
         const publicKey = keypair.publicKey.toBase58(); // 공개키를 base58로 변환합니다.
         console.log(`지갑 주소: ${publicKey}`); // 지갑 주소를 출력합니다.
-        console.log(`초기 잔액: ${initialBalance}`);
         const progress = ((i + 1) / totalKeys * 100).toFixed(2); // 처리 진행 상태를 계산합니다.
         console.log(`처리 진행 상태: ${progress}% (${i + 1}/${totalKeys})`); // 처리 상태를 출력합니다.
 
