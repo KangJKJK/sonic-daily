47c8b8c12c2305709e0ad344323420159a321804
kangjk <96231886+KangJKJK@users.noreply.github.com>
2024-08-25 22:45:21 +0900

    sonic-daily.sh

--- sonic-daily.sh
+++ sonic-daily.sh
@@ -209,21 +209,18 @@ const getUserInfo = async (auth) => {
     try {
         const response = await fetch('https://odyssey-api.sonic.game/user/rewards/info', {
             headers: {
-                'accept': '*/*',
-                'accept-language': 'en-US,en;q=0.7',
-                'content-type': 'application/json',
-                'authorization': auth,
-                'origin': 'https://odyssey.sonic.game',
-                'priority': 'u=1, i',
-                'referer': 'https://odyssey.sonic.game/',
-                'sec-ch-ua': '"Not/A)Brand";v="8", "Chromium";v="126", "Brave";v="126"',
-                'sec-ch-ua-mobile': '?0',
-                'sec-ch-ua-platform': '"Windows"',
-                'sec-fetch-dest': 'empty',
-                'sec-fetch-mode': 'cors',
-                'sec-fetch-site': 'same-site',
-                'sec-gpc': '1',
-                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'
+    'accept': '*/*',
+    'accept-language': 'en-US,en;q=0.7',
+    'content-type': 'application/json',
+    'priority': 'u=1, i',
+    'sec-ch-ua': '"Not/A)Brand";v="8", "Chromium";v="126", "Brave";v="126"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-site',
+    'sec-gpc': '1',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36'
             }
         });
 
